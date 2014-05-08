2481dc5d7afa8f749fb47567758b5bbb8c493a62
John Mitchell <jfm@minioak.com>
2014-05-08 23:30:27 +0100

    Adding Shootproof Provider

---
+++ libraries/Provider/Shootproof.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Foursquare OAuth2 Provider
+ *
+ * @package    CodeIgniter/OAuth2
+ * @category   Provider
+ * @author     Phil Sturgeon
+ * @copyright  (c) 2012 HappyNinjas Ltd
+ * @license    http://philsturgeon.co.uk/code/dbad-license
+ */
+
+class OAuth2_Provider_Shootproof extends OAuth2_Provider
+{  
+	public $method = 'GET';
+
+	public function url_authorize()
+	{
+		return 'https://auth.shootproof.com/oauth2/authorization/new';
+	}
+
+	public function url_access_token()
+	{
+		return 'https://auth.shootproof.com/oauth2/authorization/token';
+	}
+
+	public function get_user_info(OAuth2_Token_Access $token)
+	{
+		$url = 'https://api.shootproof.com/v2?'.http_build_query(array(
+			'method' => 'sp.studio.info',
+			'access_token' => $token->access_token,
+		));
+
+		$response = json_decode(file_get_contents($url));
+		
+		// Create a response from the request
+		return array(
+			'uid' => '98765433',
+			//'nickname' => $user->login,
+			'name' => $response->name
+		);
+	}
+}
